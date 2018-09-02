0febf9a319eada3b338184abeed87249545e76d4
Michelle Beshears <meeshysu@gmail.com>
2018-09-02 17:50:24 -0500

    styled css

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 let projects = [ { 
-    title: "Cool Project", 
+    title: "My Website is the Coolest Project", 
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    description: "Short description", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github 3",
     available: true,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
@@ -35,14 +35,14 @@ const printToDom = (stringToPrint, divId) => {
 const createProjectCards = () => {
     let newProject = '';
     for (i = 0; i < projects.length; i++){
-    newProject += `<div>`;
-    newProject +=   `<ul><li>${projects[i].title}</li>`;
-    newProject +=   `<li>${projects[i].screenshot}</li>`;
-    newProject +=   `<li>${projects[i].description}</li>`;
-    newProject +=   `<li>${projects[i].technologiesUsed}</li>`;
-    newProject +=   `<li>${projects[i].available}</li>`;
-    newProject +=   `<li>${projects[i].url}</li>`;
-    newProject +=   `<li>${projects[i].githubUrl}</li></ul>`;
+    newProject += `<div class='cards'>`;
+    newProject +=   `<h2>${projects[i].title}</h2>`;
+    newProject +=   `<img src='${projects[i].screenshot}'></img>`;
+    newProject +=   `<div class="content"><p>${projects[i].description}</p>`;
+    newProject +=   `<p>${projects[i].technologiesUsed}</p>`;
+    newProject +=   `<p>${projects[i].available}</p>`;
+    newProject +=   `<p>${projects[i].url}</p>`;
+    newProject +=   `<p>${projects[i].githubUrl}</div></p>`;
     newProject += `</div>`;
     if (projects[i].available === true) {
       printToDom(newProject, 'projectsPage');
