833a9ab79eba37c14a1a07d097de01154aa79cde
Michelle Beshears <meeshysu@gmail.com>
2018-09-23 15:53:39 -0500

    saving for now

--- project.js
+++ project.js
@@ -1,62 +1,62 @@
-let projects = [ { 
-    title: "My Website?", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "Short description", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github ",
-    available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  },
-  { 
-    title: "The Cheese Project", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  },
-  { 
-    title: "Cool Project", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github ",
-    available: false,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  },
-  { 
-    title: "the cool Project", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github ",
-    available: false,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  }
+let projects = [{
+  title: "My Website?",
+  screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+  description: "Short description", // A good project description includes 'the what', 'the why', and 'the how'.
+  technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github ",
+  available: true,
+  url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+  githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+},
+{
+  title: "The Cheese Project",
+  screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+  description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+  technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+  available: true,
+  url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+  githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+},
+{
+  title: "Cool Project",
+  screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+  description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+  technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github ",
+  available: false,
+  url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+  githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+},
+{
+  title: "the cool Project",
+  screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+  description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+  technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github ",
+  available: false,
+  url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+  githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+}
 ];
 
 const printToDom = (stringToPrint, divId) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = stringToPrint;
-  };
-  
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = stringToPrint;
+};
+
 const createProjectCards = () => {
-    let newProject = '';
-    for (i = 0; i < projects.length; i++){
+  let newProject = '';
+  for (i = 0; i < projects.length; i++) {
     newProject += `<div id='cards'>`;
-    newProject +=   `<h2>${projects[i].title}</h2>`;
-    newProject +=   `<img src='${projects[i].screenshot}'></img>`;
-    newProject +=   `<div class="content"><p>${projects[i].description}</p>`;
-    newProject +=   `<p>${projects[i].technologiesUsed}</p>`;
-    newProject +=   `<p>${projects[i].available}</p>`;
-    newProject +=   `<p>${projects[i].url}</p>`;
-    newProject +=   `<p>${projects[i].githubUrl}</p></div>`;
+    newProject += `<h2>${projects[i].title}</h2>`;
+    newProject += `<img src='${projects[i].screenshot}'></img>`;
+    newProject += `<div class="content"><p>${projects[i].description}</p>`;
+    newProject += `<p>${projects[i].technologiesUsed}</p>`;
+    newProject += `<p>${projects[i].available}</p>`;
+    newProject += `<p>${projects[i].url}</p>`;
+    newProject += `<p>${projects[i].githubUrl}</p></div>`;
     newProject += `</div>`;
     if (projects[i].available === true) {
       printToDom(newProject, 'projectsPage');
-    } 
-}
+    }
+  }
 };
 
 createProjectCards();
@@ -69,14 +69,20 @@ const navigationProjects = document.getElementById('navToProjects');
 
 
 bioPageBuilder = () => {
+  const flaseNavs = {navigationTech: 'false', navigationProjects: 'false'};
   const navBio = document.getElementById('navToBio').innerHTML;
   for (i = 0; i < navBio.length; i++) {
     const navBioElem = navBio[i];
-    const navBioClicked = e.target;
-    const navBioUserClicked = navBioClicked.parentNode.parentNode; //very hesitant that this is what needs to be used; keep noted//
-  if (e.target.contains('navToTechnologies' || ))
+    element.addEventListener('click', (e) => {
+      const navBioClicked = e.target;
+      const navBioUserClicked = navBioClicked.parentNode.parentNode; //very hesitant that this is what needs to be used; keep noted//
+      if (e.target.contains('navBioUserClicked' === true)) {
+        printToDom(navBioUserClicked, 'navToBio')
+      } else {
+        
+      }
+    })
   }
 }
-
 //assume a for loop?
 //if statement. if i click 'bioPage' then the other two pages should hide. 
